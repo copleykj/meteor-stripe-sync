a2d0e2ecafac60479dd297391987b39ba05515a1
Kelly <copleykj@gmail.com>
2015-06-10 16:00:58 -0400

    fix client side scripts

--- package.js
+++ package.js
@@ -1,16 +1,20 @@
 Package.describe({
-    name: "copleykj:stripe-sync",
-    summary: "Sync wrapper for stripe-node",
-    version: "2.0.0",
-    git: "https://github.com/copleykj/meteor-stripe-sync"
+    name: 'copleykj:stripe-sync',
+    summary: 'Sync wrapper for stripe-node',
+    version: '2.0.1',
+    git: 'https://github.com/copleykj/meteor-stripe-sync'
 });
 
-Npm.depends({ "stripe": "3.3.4" });
+Npm.depends({ 'stripe': '3.3.4' });
 
 Package.onUse(function(api) {
     api.versionsFrom('METEOR@0.9.0');
-    api.addFiles("includes.html", "client");
-    api.addFiles('StripeSync.js', "server");
 
-    api.export("StripeSync", "server");
+    api.use('underscore', 'server')
+    api.use('templating', 'client');
+
+    api.addFiles('includes.html', 'client');
+    api.addFiles('StripeSync.js', 'server');
+
+    api.export('StripeSync', 'server');
 });
