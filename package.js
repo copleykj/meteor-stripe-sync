af8babe7f789ed03c4afc530fa2afb612428f0d5
Kelly <copleykj@gmail.com>
2015-08-20 13:51:12 -0400

    bump version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: 'copleykj:stripe-sync',
     summary: 'Sync wrapper for stripe-node',
-    version: '2.0.4',
+    version: '2.0.5',
     git: 'https://github.com/copleykj/meteor-stripe-sync'
 });
 
